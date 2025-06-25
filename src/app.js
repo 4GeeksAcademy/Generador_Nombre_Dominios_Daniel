da6b970a2b678e321c57bcc1f9b2f41ee0500368
Dan <danielgarv2800@gmail.com>
2025-06-25 22:18:06 +0000

    arreglo en linea 16

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our', 'your', 'my'];
+let adj = ['great', 'big', 'domain', 'boring'];
+let noun = ['jogger', 'racoon', 'zebra', 'expantion'];
+let domains = ['.com', '.cr', '.org', '.go'];
+
+const combinations = () => {
+  for (let indPron = 0; indPron < pronoun.length; indPron++) {
+    const pronElm = pronoun[indPron];
+
+    for (let indAdj = 0; indAdj < adj.length; indAdj++) {
+      const adjElm = adj[indAdj];
+
+      for (let indNoun = 0; indNoun < noun.length; indNoun++) {
+        const nounElm = noun[indNoun];
+
+        for (let indDom = 0; indDom < domains.length; indDom++) {
+          const domElm = domains[indDom];
+
+          let result = pronElm + adjElm + nounElm + domElm;
+          console.log(result);
+        }
+      }
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    }
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
 };
+combinations();
