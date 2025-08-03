d01ba88e9c13f2ce2d9a2006c49f8efe46f7f8e1
Dan <danielgarv2800@gmail.com>
2025-08-03 15:02:18 +0000

    cambio de let por const, for por forEach y el dollar sign con llaves

--- src/app.js
+++ src/app.js
@@ -1,29 +1,18 @@
-let pronoun = ['the', 'our', 'your', 'my'];
-let adj = ['great', 'big', 'domain', 'boring'];
-let noun = ['jogger', 'racoon', 'zebra', 'expantion'];
-let domains = ['.com', '.cr', '.org', '.go'];
+const pronouns = ['the', 'our', 'your', 'my'];
+const adjectives = ['great', 'big', 'domain', 'boring'];
+const nouns = ['jogger', 'racoon', 'zebra', 'expantion'];
+const domains = ['.com', '.cr', '.org', '.go'];
 
 const combinations = () => {
-  for (let indPron = 0; indPron < pronoun.length; indPron++) {
-    const pronElm = pronoun[indPron];
-
-    for (let indAdj = 0; indAdj < adj.length; indAdj++) {
-      const adjElm = adj[indAdj];
-
-      for (let indNoun = 0; indNoun < noun.length; indNoun++) {
-        const nounElm = noun[indNoun];
-
-        for (let indDom = 0; indDom < domains.length; indDom++) {
-          const domElm = domains[indDom];
-
-          let result = pronElm + adjElm + nounElm + domElm;
-          console.log(result);
-        }
-      }
-
-
-    }
-  }
-
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adj => {
+      nouns.forEach(noun => {
+        domains.forEach(domain => {
+          console.log(`${pronoun}${adj}${noun}${domain}`);
+        });
+      });
+    });
+  });
 };
+
 combinations();
